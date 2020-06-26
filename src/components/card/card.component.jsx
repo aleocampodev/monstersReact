16ebfe90cbda078086e20789c594a65aabbea5e1
ocampoaleja <omaira.527@hotmail.com>
2020-06-26 13:25:57 -0500

    feature: added monsters

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,6 +3,8 @@ import './card.style.css'
 
 export const Card = (props) => (
     <div className='card-container'>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
         <h1>{props.monster.name}</h1>
+        <p>{props.monster.email}</p>
     </div>
 )
